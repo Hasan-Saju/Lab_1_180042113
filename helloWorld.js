91a515ecb0cbf8743171f1fc54c7c59a9b67eec2
Hasan-Saju <56477243+Hasan-Saju@users.noreply.github.com>
2021-05-03 23:20:10 +0600

    complete export,import, setInterval, setTimeout

---
+++ helloWorld.js
@@ -0,0 +1,14 @@
+const Hello =()=>{
+    console.log("Hello World!");
+};
+const name='Saju';
+// module.exports={HelloFunc:Hello};
+module.exports={Hello,name};
+
+// console.log(module);
+// module.exports.name='saju';
+/*
+module.exports.Hello =()=>{
+    console.log("Hello World!");
+};
+*/
---
+++ utils.js
@@ -0,0 +1,30 @@
+const HelloFunc=require('./helloWorld');
+
+// import menas that whole file execute once
+HelloFunc.Hello();
+console.log(HelloFunc.name);
+
+console.log("setInterval and Timeout");
+
+// setInterval
+
+// first parameter callback function, 2nd one time
+// 1000ms por ager function ta call hocche
+setInterval(()=>{
+    HelloFunc.Hello();
+},1000);
+
+// setTimeout
+// fixed time er por parameter function ta just ekbar call hobe
+setTimeout(()=>{
+    console.log(HelloFunc.name);
+},5000);
+
+
+
+//3 types of module
+/*
+local module
+global module
+3rd party module/package
+*/ 
