196a237afa330945d984937ed45030984772b056
Hasan-Saju <56477243+Hasan-Saju@users.noreply.github.com>
2021-06-11 00:30:18 +0600

    access data from url

--- ExpressJS/Basics/routes/userRoutes.routes.js
+++ ExpressJS/Basics/routes/userRoutes.routes.js
@@ -11,6 +11,13 @@ router.get("/login",(req,res)=>{
     res.send(`user with ID - ${id} and Username - ${username} is requesting to login.`);
 });
 
+// accessing parameters from url
+router.get("/dashboard/:id/:username",(req,res)=>{
+    const id = req.params.id;
+    const username = req.params.username;
+    res.send(`user with ID - ${id} and Username - ${username} is requesting to access.`);
+});
+
 router.get("/register",(req,res)=>{
     // res.send("This is Register Page");
     //first param -> filename, 2nd ->path, root = from the location where project started
