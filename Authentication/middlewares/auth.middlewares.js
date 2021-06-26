d511c2dc538f249cd4b414bd85ecee1636d164fa
Hasan-Saju <56477243+Hasan-Saju@users.noreply.github.com>
2021-06-27 02:41:41 +0600

    working

--- Authentication/middlewares/auth.middlewares.js
+++ Authentication/middlewares/auth.middlewares.js
@@ -1,6 +1,7 @@
 const db = require('./../database');
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcryptjs');
+const alert = require('alert');
 
 const registration = (req,res,next) => {
     
@@ -15,18 +16,37 @@ const registration = (req,res,next) => {
         if(results.length>0)
         {
             console.log("Email is already in use.");
+            alert("<h1>Email is already in use.</h1>");
+            // res.redirect("/");
+
         }
         else if(password1!=password2)
         {
-            console.log("Password doesn't match.");
+            console.log("Passwords don't match.");
+            alert("<h1>Passwords don't match.</h1>");
+            // res.redirect("/");
+            
         }
-
         let hashedPassword = await bcrypt.hash(password1,8);
         console.log(hashedPassword);
 
+        db.query('insert into auth set ?',{mail:mail,full_name:name,password:hashedPassword},(error,results)=>{
+            if(error)
+            {
+                console.log(error);
+                res.end();
+            }
+            else
+            {
+                console.log(results);
+                console.log("User registration successful.");
+                next();
+            }
+
+        });
+
     });
 
-    next();
 }
 
 module.exports = {registration};
